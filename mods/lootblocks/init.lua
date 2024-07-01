710059f14c492ab5eadc8da6c40080e4d97cce11
nurdle <50721698+nurd3@users.noreply.github.com>
2024-07-01 17:16:05 -0400

    Setup (1)

---
+++ mods/lootblocks/init.lua
@@ -0,0 +1,7 @@
+local S = minetest.get_translator"lootblocks"
+
+lootblocks = {}
+
+minetest.register_node("lootblocks:lootblock", {
+    S
+})
---
+++ mods/minitest_core/init.lua
@@ -0,0 +1,7 @@
+local S = minetest.get_translator"minitest_core"
+
+minitest_core = {}
+
+minitest_core.get_translator = S
+minitest_core.get_modpath = minetest.get_modpath"minitest_core"
+
