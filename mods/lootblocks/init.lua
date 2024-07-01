3e55f03f4cf260b2fa64876980f17013d88c6b53
nurdle <50721698+nurd3@users.noreply.github.com>
2024-07-01 17:36:28 -0400

    lootblocks

--- mods/lootblocks/init.lua
+++ mods/lootblocks/init.lua
@@ -2,6 +2,33 @@ local S = minetest.get_translator"lootblocks"
 
 lootblocks = {}
 
+lootblocks.get_translator = S
+lootblocks.get_modpath = minetest.get_modpath"lootblocks"
+
+lootblocks.registered_drops = {}
+
+function lootblocks.register_drop(stacks, chance)
+    table.insert(lootblocks.registered_drops, {stacks=stacks, chance})
+end
+
+function lootblocks.gen_drop(pos)
+    for _,v in ipairs(lootblocks.registered_drops) do
+        if math.random() < v.chance then
+            for _,w in ipairs(v.stacks) do
+                minetest.add_item(pos, w)
+            end
+        end
+    end
+end
+
 minetest.register_node("lootblocks:lootblock", {
-    S
+    title = S"Lootblock",
+    groups = {lootblock = 1},
+    drawtype = "normal",
+    tiles = {"lootblocks_lootblock.png"},
+    on_punch = function(pos, node, puncher)
+        minetest.set_node(pos, "air")
+        local inv = puncher:get_inventory()
+        lootblocks.gen_drop(pos)
+    end
 })
