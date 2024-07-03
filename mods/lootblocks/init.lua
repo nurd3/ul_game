1929499fab014c1c6fa875abd8505be4e1c87afc
nurdle <50721698+nurd3@users.noreply.github.com>
2024-07-03 08:51:07 -0400

    Setup (2)

--- mods/lootblocks/init.lua
+++ mods/lootblocks/init.lua
@@ -8,42 +8,44 @@ lootblocks.get_modpath = minetest.get_modpath"lootblocks"
 lootblocks.registered_drops = {}
 
 function lootblocks.register_drop(stacks, chance)
-    table.insert(lootblocks.registered_drops, {stacks=stacks, chance})
+	if type(stacks) == "string" then stacks = {stacks} end
+    table.insert(lootblocks.registered_drops, {stacks=stacks, chance=chance})
 end
 
 function lootblocks.gen_drop(pos)
-    local stacks = {"lootblocks:lootblock"}
+	local t = {{"lootblocks:lootblock"}}
     for _,v in ipairs(lootblocks.registered_drops) do
         if math.random() < v.chance then
-            stacks = stacks
+            table.insert(t, v.stacks)
         end
     end
-    for _,w in ipairs(v.stacks) do
+    local stacks = t[math.random(#t)]
+    for _,w in ipairs(stacks) do
         minetest.add_item(pos, ItemStack(w))
     end
 end
 
 minetest.register_node("lootblocks:lootblock", {
-    title = S"Lootblock",
+    description = S"Lootblock",
     groups = {lootblock = 1},
     drawtype = "normal",
     tiles = {"lootblocks_lootblock.png"},
     on_punch = function(pos, node, puncher)
-        minetest.set_node(pos, "air")
+        minetest.set_node(pos, {name="air"})
         local inv = puncher:get_inventory()
         lootblocks.gen_drop(pos)
-    end
+    end,
 })
 
 minetest.register_decoration({
 	name = "lootblocks:lootblock",
 	deco_type = "simple",
 	place_on = {"mapgen_stone"},
-	sidelen = 16,
+	sidelen = 1,
 	noise_params = {
-		offset = -0.012,
-		scale = 0.024,
-		spread = {x = 100, y = 100, z = 100},
+		offset = -0.25,
+		scale = 0.25,
+		spread = {x = 16, y = 16, z = 16},
 		seed = 230,
 		octaves = 3,
 		persist = 0.6
