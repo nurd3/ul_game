18b6acd3db7e11e7780bcb7709f2415aa00bec03
nurdle <50721698+nurd3@users.noreply.github.com>
2024-09-09 08:27:52 -0400

    crafting system (3)

--- mods/ul_crafting/init.lua
+++ mods/ul_crafting/init.lua
@@ -5,6 +5,8 @@ local S = minetest.get_translator"ul_crafting"
 ul_crafting.get_translator = S
 
 local recipes = {}
+local pagemax = 0
+local pagenums = {}
 
 local function compile_recipes()
     for item,_ in pairs(minetest.registered_items) do
@@ -29,18 +31,28 @@ end
 
 minetest.register_on_mods_loaded(function()
     compile_recipes()
+    pagemax = math.ceiling(#recipes / 5)
 end)
 
 sfinv.register_page(":sfinv:crafting", {
     title = S"Crafting",
 	get = function(self, player, context)
+        pagenums[player:get_player_name()] = pagenums[player:get_player_name()] or 0
+        local pagenum = pagenums[player:get_player_name()]
+        local content = ""
+        for i = pagenum * 5 + 1, pagenum * 5 + 5 do
+            content = content.."container[]"
 
+            
+
+            content = content.."container_end[]"
+        end
 		return sfinv.make_formspec(player, context,
-            page..
-            [[
-                button[1,20;5,1;back;<]
-                label[2,20;]][[]
-				listring[current_player;main]
-			]], true)
+            content..
+            "button[1,20;5,1;prev;<]"..
+            "label[1.5,21;"..pagenum.."/"..pagemax.."]"..
+            "button[2,20;5,1;next;>]"..
+			"listring[current_player;main]"
+		, true)
 	end
 })
