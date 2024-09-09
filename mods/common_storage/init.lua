85d2ec9938f6e0f216bddb084c29a29f2b672704
nurdle <50721698+nurd3@users.noreply.github.com>
2024-09-09 14:43:41 -0400

    performance improvement (1)

--- mods/common_storage/init.lua
+++ mods/common_storage/init.lua
@@ -10,19 +10,7 @@ local storage = minetest.get_mod_storage()
 
 inv:set_size("main", 4 * 4)
 
-sfinv.register_page("common_storage:common_storage", {
-    title = S"Common",
-	get = function(self, player, context)
-		return sfinv.make_formspec(player, context, [[
-				label[0.5,4.5;Changes only save when this page is closed]
-				list[detached:common_storage;main;2,0.3;4,4;]
-				listring[current_player;main]
-				listring[detached:common_storage;main]
-			]], true)
-	end
-})
-
-minetest.register_on_player_receive_fields(function(plyr, formname, fields)
+local function save()
 	
 	local size = inv:get_size("main")
 	
@@ -48,7 +36,21 @@ minetest.register_on_player_receive_fields(function(plyr, formname, fields)
 	
 	storage:set_string("items", minetest.serialize(data))
 	
-end)
+end
+
+sfinv.register_page("common_storage:common_storage", {
+    title = S"Common",
+	get = function(self, player, context)
+		return sfinv.make_formspec(player, context, [[
+				label[0.5,4.5;Changes only save when this page is closed]
+				list[detached:common_storage;main;2,0.3;4,4;]
+				listring[current_player;main]
+				listring[detached:common_storage;main]
+			]], true)
+	end,
+	on_leave = save,
+	on_player_receive_fields = save
+})
 
 
 local data = minetest.deserialize(storage:get_string("items")) or {}
