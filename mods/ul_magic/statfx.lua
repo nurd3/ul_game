a9f2bf1f9c155c51bcebe0c016fd6768d78771a0
nurdle <50721698+nurd3@users.noreply.github.com>
2024-09-12 19:02:16 -0400

    fix status effects preventing mobs from dropping on death

--- mods/ul_magic/statfx.lua
+++ mods/ul_magic/statfx.lua
@@ -1,11 +1,33 @@
+local function hp(obj, add)
+	local luaent = obj:get_luaentity()
+	
+	local hp = (luaent and (luaent.hp or luaent.health)) or obj:get_hp()
+	local hp_max = obj:get_properties().hp_max or 0
+	
+	new_hp = math.min(hp_max, 
+		math.max(hp + add, 0)
+	)
+	
+	if luaent then
+		if luaent.health then
+			luaent.health = new_hp
+		else
+			luaent.hp = new_hp
+		end
+	else
+		obj:set_hp(new_hp)
+	end
+	
+	return hp ~= new_hp
+end
+
 ul_statfx.register("ul_magic:poison", {
 	on_step = function (timer, dtime, obj)
 		if not ul_basic.is_alive(obj) then
 			return 0
 		end
 		
-		if obj:get_hp() > 1 and ul_statfx.timer(timer, dtime, 2) then
-			obj:set_hp(obj:get_hp() - 1)
+		if ul_statfx.timer(timer, dtime, 2) and hp(obj, -1) then
 			ul_basic.objsound(obj, "player_damage")
 		end
 	end
@@ -17,8 +39,7 @@ ul_statfx.register("ul_magic:burning", {
 			return 0
 		end
 		
-		if ul_statfx.timer(timer, dtime, 0.5) then
-			obj:set_hp(obj:get_hp() - 1)
+		if ul_statfx.timer(timer, dtime, 0.5) and hp(obj, -1) then
 			ul_basic.objsound(obj, "player_damage")
 		end
 	end
@@ -44,10 +65,7 @@ ul_statfx.register("ul_magic:regen", {
 			return 0
 		end
 		
-		local hp_max = obj:get_properties().hp_max or 0
-		
-		if obj:get_hp() < hp_max and ul_statfx.timer(timer, dtime, 2) then
-			obj:set_hp(obj:get_hp() + 1)
+		if ul_statfx.timer(timer, dtime, 2) and hp(obj, 1) then
 			ul_basic.objsound(obj, "ul_heal")
 		end
 	end
