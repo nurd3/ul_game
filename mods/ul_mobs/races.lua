1313636c547ff7d54d9d481ff44b04b677330ba5
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-02-07 15:00:16 -0500

    race update

--- mods/ul_mobs/races.lua
+++ mods/ul_mobs/races.lua
@@ -1,29 +1,35 @@
+local storage = core.get_mod_storage()
 local S = ul_mobs.get_translator
 
 local civ_count = 3
 local civ_relations = {}
-local civ_resources = {}
+
+local C = function(x,y)
+	return string.format("%i:%i", math.min(x, y), math.max(x, y))
+end
 
 for i = 0, civ_count do
 	for j = 0, i do
-		local x, y = math.min(i, j), math.max(i, j)
-		
-		civ_relations[tostring(x)..tostring(y)] = 0
-	end
-	if i >= 1 then
-		civ_resources[i] = {}
+		civ_relations[C(i,j)] = storage:get_int(C(i,j))
 	end
 end
 
 local function get_relations(civ1, civ2)
 	if not civ1 or not civ2 then return 0 end
-	return civ_relations[math.min(civ1, civ2) .. math.max(civ1, civ2)]
+	return civ_relations[C(civ1,civ2)] or 0
 end
 
 local function set_relations(civ1, civ2, val)
 	if not civ1 or not civ2 then return end
-	civ_relations[math.min(civ1, civ2) .. math.max(civ1, civ2)] = val
+	civ_relations[C(civ1,civ2)] = val
+	storage:set_int(C(civ1,civ2), val)
 	return val
+end
+
+local function add_relations(civ1, civ2, val)
+	if not civ1 or not civ2 then return end
+	if not val then val = 1 end
+	return set_relations(civ1, civ2, get_relations(civ1, civ2) + val)
 end
 
 local function dialogue(self, plyr)
@@ -52,6 +58,18 @@ local function monster_check(self, obj)
 	return obj:is_player() and get_relations(0, sciv) <= -2
 end
 
+local function punched(self, puncher, time_from_last_punch, tool_capabilities, dir)
+	local sciv = self.civ
+	if mobkit.is_alive(puncher) then				-- is puncher a living and alive thing
+		mobkit.hq_hunt(self, 12, puncher)			-- get revenge
+	end
+
+	mobkit_plus.on_punch(self, puncher, time_from_last_punch, tool_capabilities, dir)
+	if self.hp <= 0 and puncher:is_player() then
+		add_relations(0, sciv, -1)
+	end
+end
+
 ul_mobs.register_mob("ul_mobs:cult", {
 					-- engine values
 	description = S"Cult",
@@ -65,56 +83,61 @@ ul_mobs.register_mob("ul_mobs:cult", {
 	max_speed = 10,
 	jump_height = 1,
 	view_range = 128,
-	vision = 15,	-- how well they see in the dark
+	vision = 7,	-- how well they see in the dark
 	lung_capacity = nil,
 	max_hp = 20,
 	ranged = {
-		rate = 0.2,
+		rate = 0.5,
 		range = 128,
 		func = ul_magic.shoot,
-		args = {"ul_magic:poison", 5}
+		args = {"ul_magic:fireball", 3}
 	},
 	melee = {dmg = 2},
-	disable_fall_damage = true,
+	disable_fall_damage = false,
 	disable_taming = true,
 	
 						-- behaviour
 	on_check_prey = monster_check,
+	on_punch = punched,
 	type = "race",
 	category = "cult",
 	civ = 1
 })
-
 ul_mobs.register_mob("ul_mobs:anocula", {
 					-- engine values
 	description = S"Anocula",
-	egg_colors = {"#ffffff", "#800000"},
+	egg_colors = {"#808080", "#000000"},
 	visual = "upright_sprite",
 	textures = {"ul_mobs_race_anocula.png", "ul_mobs_race_anocula_back.png^[transformFX"},
-	visual_size = {x = 1, y = 1.5},
-	collisionbox = {-0.3, -0.75, -0.3, 0.3, 0.75, 0.3},
+	visual_size = {x = 1.5, y = 1.5},
+	collisionbox = {-0.3, -0.3, -0.3, 0.3, 0.3, 0.3},
 	
 					-- stats
-	max_speed = 15,
+	max_speed = 12,
 	jump_height = 2,
 	view_range = 128,
-	vision = 15,	-- how well they see in the dark
+	vision = 10,	-- how well they see in the dark
 	lung_capacity = nil,
-	max_hp = 20,
-	ranged = {
-		rate = 0.2,
-		range = 128,
-		func = ul_magic.shoot,
-		args = {"ul_magic:fireball", 5}
-	},
-	melee = {dmg = 4},
+	max_hp = 15,
+	melee = {dmg = 3},
 	disable_fall_damage = true,
 	disable_taming = true,
 	
 						-- behaviour
 	on_check_prey = monster_check,
+	on_punch = punched,
 	type = "race",
 	category = "alien",
 	civ = 2
 })
 
+local timer = 0
+core.register_globalstep(function(dtime)
+	timer = timer + dtime
+	if timer >= 1.0 then
+		for k,v in pairs(civ_relations) do
+			-- civ_relations slowly approach 0
+			civ_relations[k] = (v == 0 and 0) or (v > 0 and v - 1) or (v + 1)
+		end
+	end
+end)
